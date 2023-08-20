e20d83cf3ecc3a7edd50e93a00f7c8eb3a19764b
Nick K <nick.kruchinin@gmail.com>
2023-08-20 21:38:53 +0300

    Small fixes for paths

--- server/utils/withORM.ts
+++ server/utils/withORM.ts
@@ -3,7 +3,7 @@ import "reflect-metadata"
 import {RequestContext} from "@mikro-orm/core"
 import type {EventHandler} from "h3"
 
-import {getORM} from "~/server/lib/db/orm"
+import {getORM} from "../lib/db/orm.js"
 
 export const withORM = (handler: EventHandler) => defineEventHandler(
   async event => {
